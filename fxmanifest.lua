bfba8484de9d13c1efb5c14dc29d4c1e990b67ca
borisnliscool <60477582+borisnliscool@users.noreply.github.com>
2023-10-13 18:22:26 +0200

    update manifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,12 @@
 fx_version "cerulean"
 game "gta5"
 
+name "Ding"
+description "Ding protects FiveM events from cheaters using random nonces to prevent replay attacks."
+author "borisnliscool"
+version "1.0.0"
+repository "https://github.com/borisnliscool/ding"
+
 files {
     "src/utils.lua",
     "src/client.lua",
